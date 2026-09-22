de4c124175a64f1ebb3825158e1cb5d9efef3deb
Swaraj kumar Behera <23053092@kiit.ac.in>
2026-09-22 20:38:18 +0000

    hello world

---
+++ productshop/srv/service.js
@@ -0,0 +1,14 @@
+const cds=require('@sap/cds');
+
+module.exports=cds.service.impl(async function(srv){
+    srv.on('printhelloworld',req =>{
+        console.log(req.data.input)
+        return `${req.data.input} World`
+    })
+})
+
+// req={
+//     "data" : {
+//         "input" : "Hello"
+//     }
+// }
