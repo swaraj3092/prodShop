eb5f707100108b802b67fba2ab4849c2dfd53b7d
Swaraj kumar Behera <23053092@kiit.ac.in>
2026-09-22 21:47:58 +0000

    rest and odata

--- productshop/srv/service.cds
+++ productshop/srv/service.cds
@@ -1,3 +1,9 @@
+//@protocol:'rest'
 service product{
     function printhelloworld(input: String) returns String;
+    function addition(num1: Integer,num2: Integer) returns Integer;
+
+    @open
+    type object{};
+    function MyFunction(category:Integer) returns object;
 }
--- productshop/srv/service.js
+++ productshop/srv/service.js
@@ -5,6 +5,37 @@ module.exports=cds.service.impl(async function(srv){
         console.log(req.data.input)
         return `${req.data.input} World`
     })
+
+    srv.on('addition',req=>{
+        console.log(req.data)
+        let result=req.data.num1+req.data.num2;
+
+        return result
+    })
+
+    srv.on('MyFunction',req=>{
+        let result={}
+
+        if(req.data.category == 1){
+            result.product='BMW',
+            result.price='1200 USD',
+            result.location='NEW DELHI'
+        }
+        else{
+            result.product='AUDI',
+            result.stock= 120,
+            result.priceArray=[
+                {
+                    "price":47457,
+                    "Discount": "20%"
+                }
+            ]
+        }
+
+        return result;
+    })
+
+
 })
 
 // req={
